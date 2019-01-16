1d0e74b665095c6a6ddac217e9b4f89ebcce170d
Dan Spencer <plinkos@gmail.com>
2019-01-16 09:08:02 -0500

    Update pod spec

---
+++ lib/settingsfrompullrequestwithandroidlocationmenu.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class SettingsFromPullRequestWithAndroidLocationMenu {
+  static const MethodChannel _channel =
+      const MethodChannel('plugins.ly.com/settings');
+
+  static Future<bool> openWiFiSettings() async {
+    return await _channel.invokeMethod('wifi');
+  }
+
+  static Future<bool> openGPSSettings() async {
+    return await _channel.invokeMethod('gps');
+  }
+}
